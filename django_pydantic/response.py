9e60a07ac6eff4ed096b8454f42e6f231ec9de28
NEFORCEO <pirogartem919@gmail.com>
2026-04-29 21:29:30 +0700

    feat: create response.py

---
+++ django_pydantic/response.py
@@ -0,0 +1,18 @@
+from __future__ import annotations
+
+import json
+from typing import Any, Generic, TypeVar
+
+from django.http import JsonResponse
+from pydantic import BaseModel
+
+_M = TypeVar("_M", bound=BaseModel)
+
+
+class ModelResponse(JsonResponse, Generic[_M]):
+    def __init__(self, model: _M, status: int = 200, **kwargs: Any) -> None:
+        super().__init__(
+            json.loads(model.model_dump_json()),
+            status=status,
+            **kwargs,
+        )
